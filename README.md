b578f6cf775a3ffe5b251c2fcd94f3ff8e4b4e4e
Simone de Blasiis <simone.deblasiis@gmail.com>
2025-07-30 21:17:58 +0200

    Polished the README

--- README.md
+++ README.md
@@ -1,25 +1,42 @@
-# Niu E-scooter Home Assistant integration
+# NIU E-scooter Home Assistant integration
 
-This is a custom component for Home Assistant to integrate your Niu Scooter.
+This is a custom component for Home Assistant to integrate your NIU Scooter.
 
-Now this integration is _asynchronous_ and it is easy installable via config flow.
+The integration can be setup from the GUI.
+
+## Features:
+
+- You can select which sensors to fetch from NIU's APIs
+- You can turn your scooter on and off directly from Home Assistant
+- If you enable the Last Track sensor you'll get a camera entity that will show your scooter's last track
 
 ## Changes:
-* Now it will generate automatically a Niu device so all the sensors and the camera will grouped
-![auto device](images/niu_integration_device.png)
-* If you select the Last track sensor automatically it will create a camera integration, with the rendered image of your last track.
-![last track camera](images/niu_integration_camera.png)
+
+- The integration will now ask you to setup a language as this affects the language of the notifications that NIU will send on their mobile apps
+- Instead of getting a token for each request to NIU's APIs we now store it and renew it only when it is necessary, hopefully this means we'll be able to incur less in rate limiting
+- You will now be able to turn your scooter on and off directly from Home Assistant
+
+## Some pictures:
+
+### Device example
+
+![Device example](images/integration_device_example.png)
+
+### Camera entity
+
+![Last track camera](images/niu_integration_camera.png)
 
 With the thanks to pikka97 !!!
 
 ## Setup
-1. In Home Assistant's settings under "device and services" click on the "Add integration" button.
-2. Search for "Niu Scooters" and click on it.
-3. Insert your Niu app companion's credentials and select which sensors do you want.
-![config flow](images/config_flow_niu_integration.png)
-4. Enjoy your new Niu integration :-)
+
+![Configuration flow](images/integration_config_flow_example.png)
+
+1. In Home Assistant's settings under "Device and services" click on the "Add integration" button.
+2. Search for "NIU Scooters" and click on it.
+3. Insert your NIU app companion's credentials, select a language which sensors you'd like to enable.
+4. Enjoy your new NIU integration :-)
 
 ## Known bugs
 
-some people had problems with this version please take the latest 1.o  versions
-See https://github.com/marcelwestrahome/home-assistant-niu-component repository
+None but if you encounter any please let me know
--- custom_components/niu/api.py
+++ custom_components/niu/api.py
@@ -17,7 +17,9 @@ _LOGGER = logging.getLogger(__name__)
 
 
 class NiuApi:
-    def __init__(self, username, password, scooter_id, language, hass=None, entry=None) -> None:
+    def __init__(
+        self, username, password, scooter_id, language, hass=None, entry=None
+    ) -> None:
         self.username = username
         self.password = password
         self.scooter_id = int(scooter_id)
@@ -29,7 +31,7 @@ class NiuApi:
         self.dataMoto = None
         self.dataMotoInfo = None
         self.dataTrackInfo = None
-        
+
         # Token management
         self.token = None
         self.token_expires_at = None
@@ -37,12 +39,12 @@ class NiuApi:
     def initApi(self):
         # Load stored token if available
         self._load_stored_token()
-        
+
         # Get or refresh token
         if not self._is_token_valid():
             self.token = self.get_token()
             # Don't save token here - will be saved from main thread
-        
+
         api_uri = MOTOINFO_LIST_API_URI
         self.sn = self.get_vehicles_info(api_uri)["data"]["items"][self.scooter_id][
             "sn_id"
@@ -73,21 +75,21 @@ class NiuApi:
         except BaseException as e:
             _LOGGER.error(f"Error getting token: {e}")
             return False
-        
+
         if r.status_code != 200:
             _LOGGER.error(f"Token request failed with status code: {r.status_code}")
             return False
-            
+
         try:
             data = json.loads(r.content.decode())
             token_data = data["data"]["token"]
             access_token = token_data["access_token"]
-            
+
             # Calculate expiration time (assume 24 hours if not provided)
             # NIU tokens typically last for 24 hours
             expires_in = token_data.get("expires_in", 86400)  # Default to 24 hours
             self.token_expires_at = time.time() + expires_in
-            
+
             _LOGGER.debug("Successfully obtained new token")
             return access_token
         except (KeyError, json.JSONDecodeError) as e:
@@ -100,46 +102,46 @@ class NiuApi:
             token_data = self.entry.data["token_data"]
             self.token = token_data.get("access_token")
             self.token_expires_at = token_data.get("expires_at")
-            _LOGGER.debug("Loaded stored token that expires at %s", self.token_expires_at)
-            
+            _LOGGER.debug(
+                "Loaded stored token that expires at %s", self.token_expires_at
+            )
+
     async def async_save_token(self):
         """Async method to save token from main event loop."""
         if self.hass and self.entry and self.token:
             token_data = {
                 "access_token": self.token,
-                "expires_at": self.token_expires_at
+                "expires_at": self.token_expires_at,
             }
-            
+
             # Update the config entry with new token data
             new_data = dict(self.entry.data)
             new_data["token_data"] = token_data
-            
-            self.hass.config_entries.async_update_entry(
-                self.entry, data=new_data
-            )
+
+            self.hass.config_entries.async_update_entry(self.entry, data=new_data)
             _LOGGER.debug("Saved token to config entry")
-            
+
     def has_unsaved_token(self):
         """Check if there's a new token that needs to be saved."""
         if not self.token or not self.hass or not self.entry:
             return False
-            
+
         stored_token_data = self.entry.data.get("token_data", {})
         stored_token = stored_token_data.get("access_token")
-        
+
         return self.token != stored_token
-            
+
     def _is_token_valid(self):
         """Check if the current token is valid and not expired."""
         if not self.token or not self.token_expires_at:
             return False
-            
+
         # Add 5 minute buffer before expiration
         buffer_time = 300  # 5 minutes
         current_time = time.time()
-        
+
         return current_time < (self.token_expires_at - buffer_time)
-        
+
     def _ensure_valid_token(self):
         """Ensure we have a valid token, refresh if needed."""
         if not self._is_token_valid():
@@ -156,7 +158,7 @@ class NiuApi:
     def get_vehicles_info(self, path):
         if not self._ensure_valid_token():
             return False
-            
+
         token = self.token
         url = API_BASE_URL + path
         headers = {"token": token}
@@ -175,7 +177,7 @@ class NiuApi:
     ):
         if not self._ensure_valid_token():
             return False
-            
+
         sn = self.sn
         token = self.token
         url = API_BASE_URL + path
@@ -184,7 +186,9 @@ class NiuApi:
         params = {"sn": sn}
         headers = {
             "token": token,
-            "User-Agent": "manager/5.5.8 (android; SM-S918B 14);lang=" + language + ";clientIdentifier=Overseas;timezone=Europe/Rome;model=samsung_SM-S918B;deviceName=SM-S918B;ostype=android",
+            "User-Agent": "manager/5.5.8 (android; SM-S918B 14);lang="
+            + language
+            + ";clientIdentifier=Overseas;timezone=Europe/Rome;model=samsung_SM-S918B;deviceName=SM-S918B;ostype=android",
         }
         try:
             r = requests.get(url, headers=headers, params=params)
@@ -204,7 +208,7 @@ class NiuApi:
     ):
         if not self._ensure_valid_token():
             return False
-            
+
         sn, token = self.sn, self.token
         url = API_BASE_URL + path
         params = {}
@@ -219,7 +223,7 @@ class NiuApi:
         if data["status"] != 0:
             return False
         return data
-    
+
     def post_ignition(
         self,
         path,
@@ -227,15 +231,17 @@ class NiuApi:
     ):
         if not self._ensure_valid_token():
             return False
-            
+
         sn, token, language = self.sn, self.token, self.language
         url = API_BASE_URL + path
         params = {}
         headers = {
             "token": token,
             "Content-Type": "application/json",
-            "User-Agent": "manager/5.5.8 (android; SM-S918B 14);lang=" + language + ";clientIdentifier=Overseas;timezone=Europe/Rome;model=samsung_SM-S918B;deviceName=SM-S918B;ostype=android"
-            }
+            "User-Agent": "manager/5.5.8 (android; SM-S918B 14);lang="
+            + language
+            + ";clientIdentifier=Overseas;timezone=Europe/Rome;model=samsung_SM-S918B;deviceName=SM-S918B;ostype=android",
+        }
         ignitionParam = "acc_off"
         if ignition == True:
             ignitionParam = "acc_on"
@@ -253,7 +259,7 @@ class NiuApi:
     def post_info_track(self, path):
         if not self._ensure_valid_token():
             return False
-            
+
         sn, token = self.sn, self.token
         url = API_BASE_URL + path
         params = {}
@@ -278,7 +284,7 @@ class NiuApi:
             return False
         return data
 
-    def getDataBat(self, id_field): 
+    def getDataBat(self, id_field):
         return self.dataBat["data"]["batteries"]["compartmentA"][id_field]
 
     def getDataMoto(self, id_field):
@@ -321,76 +327,3 @@ class NiuApi:
 
     def setIgnition(self, ignition):
         return self.post_ignition(IGNITION_URI, ignition)
-
-
-"""class NiuDataBridge(object):
-    async def __init__(self, api):
-    #  hass, username, password, country, scooter_id):
-
-        self.api = api
-        # await hass.async_add_executor_job(lambda : NiuDataBridge(username, password, country, scooter_id))
-        # NiuApi(username, password, country, scooter_id)
-        sn, token = self.api.sn, self.api.token
-
-        self._dataBat = None
-        self._dataMoto = None
-        self._dataMotoInfo = None
-        self._dataTrackInfo = None
-        self._sn = sn
-        self._token = token
-
-    def token(self):
-        return self.api.token
-    
-    def sn(self):
-        return self.api.sn
-
-    def sensor_prefix(self):
-        return self.api.sensor_prefix
-
-    def dataBat(self, id_field):
-        return self._dataBat["data"]["batteries"]["compartmentA"][id_field]
-
-    def dataMoto(self, id_field):
-        return self._dataMoto["data"][id_field]
-
-    def dataDist(self, id_field):
-        return self._dataMoto["data"]["lastTrack"][id_field]
-
-    def dataPos(self, id_field):
-        return self._dataMoto["data"]["postion"][id_field]
-
-    def dataOverall(self, id_field):
-        return self._dataMotoInfo["data"][id_field]
-
-    def dataTrack(self, id_field):
-        if id_field == "startTime" or id_field == "endTime":
-            return datetime.fromtimestamp(
-                (self._dataTrackInfo["data"][0][id_field]) / 1000
-            ).strftime("%Y-%m-%d %H:%M:%S")
-        if id_field == "ridingtime":
-            return strftime(
-                "%H:%M:%S", gmtime(self._dataTrackInfo["data"][0][id_field])
-            )
-        if id_field == "track_thumb":
-            thumburl = self._dataTrackInfo["data"][0][id_field].replace(
-                "app-api.niucache.com", "app-api-fk.niu.com"
-            )
-            return thumburl.replace("/track/thumb/", "/track/overseas/thumb/")
-        return self._dataTrackInfo["data"][0][id_field]
-
-    @Throttle(timedelta(seconds=1))
-    def updateBat(self):
-        self._dataBat = self.api.get_info(MOTOR_BATTERY_API_URI)
-
-    @Throttle(timedelta(seconds=1))
-    def updateMoto(self):
-        self._dataMoto = self.api.get_info(MOTOR_INDEX_API_URI)
-
-    @Throttle(timedelta(seconds=1))
-    def updateMotoInfo(self):
-        self._dataMotoInfo = self.api.post_info(MOTOINFO_ALL_API_URI)
-
-    @Throttle(timedelta(seconds=1))
-    def updateTrackInfo(self):
-        self._dataTrackInfo = self.api.post_info_track(TRACK_LIST_API_URI)"""
